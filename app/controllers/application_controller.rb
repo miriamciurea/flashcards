eaac2f6e3a8ce25399fb0f3d1ebd113a08b515d7
Miriam Ciurea <miriamciurea777@icloud.com>
2024-05-01 20:11:01 +0300

    flashes + username (idk what was the problem, but I solved it :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  # end
+
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_params = %i[username]
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: devise_params)
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: devise_params)
   end
 
 end
